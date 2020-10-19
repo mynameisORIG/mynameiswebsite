57f92f1928252ce334f982e6e6b0d9e806d1b74f
mynameisORIG <57204473+mynameisORIG@users.noreply.github.com>
2020-10-19 19:32:03 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,1 @@
-document.getElementById("menu").innerHTML += "<div class="topnav">
-			  <a class="active" href="#home">Home</a>
-			  <a href="projects.html">GitHub Projects</a>
-			  <a href="Resume">Resume</a>
-			  <a href="HTB.html">HTB Notes</a>
-			  <a href="papers.html">Papers</a>
-		</div>";
+document.getElementById("menu").innerHTML += "<div class="topnav"> <a class="active" href="#home">Home</a> <a href="projects.html">GitHub Projects</a> <a href="Resume">Resume</a> <a href="HTB.html">HTB Notes</a> <a href="papers.html">Papers</a> </div>";
