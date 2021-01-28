a0d2d65ed8d8275d8c6682a6eac0a750e8eabc84
mynameisORIG <57204473+mynameisORIG@users.noreply.github.com>
2021-01-28 17:29:02 -0500

    Delete menu.js

--- menu.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("menu").innerHTML += "<div class="topnav"> <a class="active" href="index.html">Home</a> <a href="projects.html">GitHub Projects</a> <a href="Resume">Resume</a> <a href="HTB.html">HTB Notes</a> <a href="papers.html">Papers</a> </div>";
